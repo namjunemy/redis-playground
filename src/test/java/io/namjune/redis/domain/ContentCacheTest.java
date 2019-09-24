97be3a035008b878ccd6d8458a8485168197c894
Namjun Kim <namjunemy@gmail.com>
2019-09-24 15:02:51 +0900

    redisson I/O시 코덱 설정

--- src/test/java/io/namjune/redis/domain/ContentCacheTest.java
+++ src/test/java/io/namjune/redis/domain/ContentCacheTest.java
@@ -1,20 +1,22 @@
 package io.namjune.redis.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.LongStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.LongStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -42,22 +44,28 @@ public class ContentCacheTest {
         putTestData(size);
 
         //when
-        RMap<Long, List<Long>> map = this.redissonClient.getMap(TEST_KEY);
+        RMap<Long, List<Long>> map = this.redissonClient.getMap(TEST_KEY, new JsonJacksonCodec());
         ContentCache contentCache = ContentCache.builder()
             .contentsByCpIdxMap(map.readAllMap())
             .build();
+
+        Iterable<String> keys = redissonClient.getKeys().getKeysByPattern(TEST_KEY);
+        boolean isExistKey = keys.iterator().hasNext();
 
         //then
         assertThat(map.size()).isEqualTo(size);
         assertThat(contentCache.getContentsByCpIdxMap().size()).isEqualTo(size);
         assertThat(contentCache.getContentsByCpIdxMap().get(1L).size()).isEqualTo(5);
         assertThat(contentCache.getContentsByCpIdxMap().get(3L).get(0)).isEqualTo(3L);
+        assertThat(isExistKey).isTrue();
     }
 
     private void putTestData(int size) {
+        Map<Long, List<Long>> testMap = new HashMap<>();
         LongStream.rangeClosed(1L, size)
-            .forEach(l -> this.redissonClient
-                .getMap(TEST_KEY)
+            .forEach(l -> testMap
                 .put(l, Arrays.asList(l, 2L * l, 3L * l, 4L * l, 5L * l)));
+        this.redissonClient.getMap(TEST_KEY, new JsonJacksonCodec())
+            .putAllAsync(testMap);
     }
 }
